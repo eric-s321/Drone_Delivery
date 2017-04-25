061eec85dff26dee3c0ee4c93b2bb129844b6379
Eric Scagnelli <ericscagnelli@gmail.com>
2017-04-25 01:35:22 -0400

    Set up MissionWindow class and start adding tkinter to create GUI. Initial screen done

--- MissionGenerator.py
+++ MissionGenerator.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from tkinter import messagebox
+
 TOP_HEIGHT = 20 #meters above the coordinate
 BOTTOM_HEIGHT = 3 #meters above the coordinate
 COORD_FRAME = 3
@@ -46,14 +49,79 @@ class MissionGenerator:
         self.index += 1
         self.file.write(line)
         
-
 class Coordinate:
     def __init__(self, longitude, latitude, altitude):
         self.longitude = longitude
         self.latitude = latitude
         self.altitude = altitude
 
+class MissionWindow:
+
+    def __init__(self, root):
+        self.root = root
+        self.frame1 = Frame(self.root)
+        self.mainFrame = Frame(self.root)
+        self.frame1.pack()
+        self.coords = []
+        self.fields = []
+        self.initialSetup()
+
+    def initialSetup(self):
+        Label(self.frame1, text="Number of Waypoints:").grid(row=0, column=0) 
+        Label(self.frame1, text="File Name:").grid(row=1,column=0)
+        self.fields.append(Entry(self.frame1))
+        self.fields.append(Entry(self.frame1))
+        self.fields[0].grid(row=0,column=1)
+        self.fields[1].grid(row=1,column=1)
+        Button(self.frame1, text="Continue", command=self.mainWindowTransition).grid(
+                row=2, column=1)
+
+    def mainWindowSetup(self):
+        r = 0
+        c = 0
+        Label(self.mainFrame, text="Home Coordinates").grid(row=r, column=c)
+        r += 1
+        for field in self.fields:
+            field.grid(row=r,column=c)
+            c += 1
+
+    def mainWindowTransition(self):
+        validInput = True
+        try:
+            self.numWaypoints = int(self.fields[0].get())
+            if self.numWaypoints < 1 or self.numWaypoints > 8:
+                raise ValueError
+        except ValueError:
+            validInput = False
+            messagebox.showinfo("Error", "Number of waypoints must be an integer between 1 and 8")
+
+        if validInput:
+            self.fileName = self.fields[1].get()
+
+            #remove frame1 
+            self.frame1.grid_forget()
+            self.frame1.destroy()
+
+            #clear fields list
+            self.fields = []
+            self.addTextFields(self.numWaypoints * 3 + 3)
+            self.mainWindowSetup()
+
+            self.mainFrame.pack()
+
+    def addTextFields(self, n):
+        for i in range(n):
+            self.fields.append(Entry(self.mainFrame))
+
+
 if __name__ == '__main__':
+
+    root = Tk()
+    root.minsize(width=500,height=500)
+    root.title('Mission Generator')
+    window = MissionWindow(root)
+    root.mainloop()
+
     numWaypoints = int(input('How many waypoint?\n'))
     fileName = input('What do you want to call the waypoint file?\n')
     coords = []
